281fe7a16dce793b6deba6dce86d643143c914e0
ominyoung <ohmj090747@gmail.com>
2022-01-26 14:33:31 +0900

    카테고리 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('<int:pk>/', views.single_post_page),
+    # path('<int:pk>/', views.single_post_page),
     # path('',views.index),
+    path('category/<str:slug>/', views.category_page),
     path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view())
 ]
